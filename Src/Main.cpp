c8beecbaea5330bb6069d6663e64dc46b31c79c6
nthrongtrair <nathakorn.throngtrairat@agoda.com>
2022-06-10 02:30:33 +0700

    basic init gl window

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+void FramebufferSizeCallback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
 
 int main()
 {
     const int res = glfwInit();
-    printf("Hello world %d", res);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // For Mac machine
+
+    GLFWwindow *Window = glfwCreateWindow(800, 600, "Graphics", NULL, NULL);
+    if (Window == NULL)
+    {
+        const char *description;
+        auto err = glfwGetError(&description);
+        printf("Failed to init gl window %d\n%s", err, description);
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(Window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(Window, FramebufferSizeCallback);
+
     return 0;
 }
