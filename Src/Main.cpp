2d7904c4bd027c1ee40c02ce92aebea01f9f7c65
Boss <bossnathakorn@gmail.com>
2022-06-10 21:45:51 +0700

    simplify master branch for init only

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -2,8 +2,6 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include "TraingleRender.h"
-
 void FramebufferSizeCallback(GLFWwindow *window, int width, int height)
 {
     glViewport(0, 0, width, height);
@@ -77,56 +75,6 @@ int main()
         return -1;
     }
 
-    // Init and compile Shaders and ShaderProgram
-    const unsigned int shaderProgram = glCreateProgram();
-    const unsigned int vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
-    const unsigned int fragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
-
-    CompileShader(vertexShaderId, fragmentShaderId);
-    ReportShaderCompileStatus(vertexShaderId);
-    ReportShaderCompileStatus(fragmentShaderId);
-
-    // Attach and link shaders to program
-    glAttachShader(shaderProgram, vertexShaderId);
-    glAttachShader(shaderProgram, fragmentShaderId);
-    glLinkProgram(shaderProgram);
-
-    int success;
-    char *infoLog = nullptr;
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if(!success)
-    {
-        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
-    }
-
-    // Delete shaders as we already linked it to program
-    glDeleteShader(vertexShaderId);
-    glDeleteShader(fragmentShaderId);
-
-    // Init VAO to use with VBO
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
-
-    // Init VBO for storing vertices data
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-
-    // Bind VBO to array buffer
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-    // Specify vertex data to be stored in VBO
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    Update();
-
-    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    glBindVertexArray(0);
-
     while (!glfwWindowShouldClose(window))
     {
         ProcessInput(window);
@@ -135,20 +83,9 @@ int main()
         glClearColor(.3f, .3f, .3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Update and draw triangle
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-
-    // optional: de-allocate all resources once they've outlived their purpose:
-    // ------------------------------------------------------------------------
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteProgram(shaderProgram);
 
     glfwTerminate();
     return 0;
