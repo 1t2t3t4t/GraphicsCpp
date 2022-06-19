bf70fe051f0b8cbaee52118f4c59df2af1e225ff
Boss <bossnathakorn@gmail.com>
2022-06-19 16:16:17 +0700

    impl cam movement

---
+++ Src/Rotator.cpp
@@ -0,0 +1,16 @@
+//
+// Created by bossb on 19/06/2022.
+//
+
+#include "Rotator.h"
+
+#include <glm/glm.hpp>
+
+glm::vec3 Rotator::ToVec3() const
+{
+    const auto x = glm::cos(glm::radians(Yaw)) * glm::cos(glm::radians(Pitch));
+    const auto y = glm::sin(glm::radians(Pitch));
+    const auto z = glm::sin(glm::radians(Yaw));
+    glm::vec3 result(x, y, z);
+    return glm::normalize(result);
+}
